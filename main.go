265442368ea9b1372e2e2b119cb7b651748c51c0
Gyeongmin Go <gkm2164@gmail.com>
2021-04-22 00:57:58 -0700

    include assets

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"chat-server/ws"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net/http/httputil"
-	"net/url"
 )
 
 func main() {
@@ -15,18 +14,8 @@ func main() {
 		ws.Chat(c.Writer, c.Request)
 	})
 
-	server.NoRoute(func(c *gin.Context) {
-		u, _ := url.Parse("http://localhost:3000/")
-
-		proxy := httputil.NewSingleHostReverseProxy(u)
-
-		c.Request.URL.Host = u.Host
-		c.Request.URL.Scheme = u.Scheme
-		c.Request.Header.Set("X-Forwarded-Host", c.Request.Header.Get("Host"))
-		c.Request.Host = u.Host
-
-		proxy.ServeHTTP(c.Writer, c.Request)
-	})
+	server.NoRoute(static.Serve("/",
+		static.LocalFile("./assets", false)))
 
 	if err := server.Run(":8081"); err != nil {
 		log.Fatalf("error while starting server: %+v", err)
