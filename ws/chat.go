bb4c0c3459bc396b4c1904fe69eb1033e9e838aa
Gyeongmin Go <gyeongm@amazon.com>
2021-04-22 11:39:05 -0700

    update

--- ws/chat.go
+++ ws/chat.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -29,36 +30,38 @@ func Chat(w gin.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	if conn, err := ws.Upgrade(w, r, nil); err != nil {
+	conn, err := ws.Upgrade(w, r, nil)
+	if err != nil {
 		fmt.Println(err)
 		return
-	} else {
-		if _, ok := users[conn]; !ok {
-			users[conn] = util.RandStringBytes(8)
-		}
+	}
 
-		name := users[conn]
-		msgCh <- JoinMessage{
+	if _, ok := users[conn]; !ok {
+		users[conn] = util.RandStringBytes(8)
+	}
+
+	name := users[conn]
+	msgCh <- JoinMessage{
+		Name: name,
+		Conn: conn,
+	}
+
+	defer func(conn *websocket.Conn, name string) {
+		msgCh <- LeaveMessage{
 			Name: name,
 			Conn: conn,
 		}
+	}(conn, name)
 
-		defer func(conn *websocket.Conn, name string) {
-			msgCh <- LeaveMessage{
-				Name: name,
-				Conn: conn,
-			}
-		}(conn, name)
+	for {
+		msg := ""
+		if err := conn.ReadJSON(&msg); err != nil {
+			break
+		}
 
-		for {
-			msg := ""
-			if err := conn.ReadJSON(&msg); err != nil {
-				break
-			}
-
-			msgCh <- MessageSend{
-				Message: fmt.Sprintf("%s: %s", name, msg),
-			}
+		msgCh <- MessageSend{
+			Sender: name,
+			Message: msg,
 		}
 	}
 }
@@ -71,6 +74,7 @@ type JoinMessage struct {
 }
 
 type MessageSend struct {
+	Sender  string
 	Message string
 }
 
@@ -83,6 +87,14 @@ func Now() string {
 	return time.Now().Format("15:04:05")
 }
 
+func broadcastMessage(m map[string]*websocket.Conn, message string) {
+	for _, conn := range m {
+		if err := conn.WriteJSON(message); err != nil {
+			continue
+		}
+	}
+}
+
 func Handler(msgCh chan ChatMessageDetail) {
 	var m = map[string]*websocket.Conn{}
 
@@ -91,31 +103,61 @@ func Handler(msgCh chan ChatMessageDetail) {
 		case JoinMessage:
 			joinMsg := msg.(JoinMessage)
 			m[joinMsg.Name] = joinMsg.Conn
-			for _, conn := range m {
-				if err := conn.WriteJSON(fmt.Sprintf("[%s] %s joined room", Now(), joinMsg.Name)); err != nil {
-					continue
-				}
-			}
+			broadcastMessage(m, fmt.Sprintf("[%s] %s joined room", Now(), joinMsg.Name))
 			break
 		case MessageSend:
 			sendMsg := msg.(MessageSend)
-			for _, conn := range m {
-				if err := conn.WriteJSON(fmt.Sprintf("[%s] %s", Now(), sendMsg.Message)); err != nil {
-					continue
+			if strings.HasPrefix(sendMsg.Message, "/") {
+				conn := m[sendMsg.Sender]
+				var message string
+				if ret, err := parseMessage(sendMsg.Message, m); err != nil {
+					message = fmt.Sprintf("error while parse your command: %v", err)
+				} else {
+					message = ret
 				}
+
+				if err := conn.WriteJSON(fmt.Sprintf("SYSTEM: %s", message)); err != nil {
+					break
+				}
+			} else {
+				broadcastMessage(m, fmt.Sprintf("[%s] %s: %s", Now(), sendMsg.Sender, sendMsg.Message))
 			}
 			break
 		case LeaveMessage:
 			leaveMsg := msg.(LeaveMessage)
 			delete(m, leaveMsg.Name)
-			for _, conn := range m {
-				if err := conn.WriteJSON(fmt.Sprintf("[%s] %s left room", Now(), leaveMsg.Name)); err != nil {
-					continue
-				}
-			}
+			broadcastMessage(m, fmt.Sprintf("[%s] %s left room", Now(), leaveMsg.Name))
 			break
 		default:
 			fmt.Printf("unknown message: %v", msg)
 		}
+	}
+}
+
+func parseMessage(message string, m map[string]*websocket.Conn) (string, error) {
+	var command = ""
+	for i := 1; i < len(message); i++ {
+		if message[i] == ' ' {
+			command = message[1:i]
+			message = message[i + 1:]
+		}
+	}
+
+	if command == "" {
+		command = message[1:]
+	}
+
+	switch command {
+	case "members":
+		var members []string
+		for member, _ := range m {
+			members = append(members, member)
+		}
+		return fmt.Sprintf("[%s] List of members %d: [%s]",
+			Now(), len(members), strings.Join(members, ", ")), nil
+	case "whisper":
+		fallthrough
+	default:
+		return "", fmt.Errorf("unknown command: %v", command)
 	}
 }
